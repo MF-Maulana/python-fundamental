13566cf67d4d8ada7e0192b7039293c3a24a2005
muhfirmanmaulana <muhammadfm.al@gmail.com>
2023-11-20 15:29:51 +0700

    Perulangan dengan while with undefined count

--- dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -18,3 +18,8 @@ while total_jumlah_baca < jumlah_buku * 2:
         print(f'Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami')
 
 print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+    print('"Bu, semua buku sudah dibaca dan dipahami"')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. '
+          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku"')
